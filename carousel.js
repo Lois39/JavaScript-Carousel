6328305251fc2bd508bd26effd9ccc3ff4dacc9b
Okereke Lois <okerekelois2@gmail.com>
2022-11-11 02:31:56 -0800

    added changes by Lois

--- carousel.js
+++ carousel.js
@@ -1,10 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 
 const swiper = new Swiper('.swiper', {
-    autoplay: {
-       delay: 3000,
-       disableOnInteraction: false,
-    },
+    
    loop: true,
  
    // If we need pagination
